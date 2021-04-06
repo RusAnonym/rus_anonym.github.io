834e84b1945646fff8b38893ddd103d6bc100401
RusAnonym <alexandrsemin2033@gmail.com>
2021-04-06 10:37:48 +0300

    remove context menu on right click

---
+++ src/TS/scripts/events.ts
@@ -0,0 +1,5 @@
+window.document.addEventListener("contextmenu", (event) =>
+	event.preventDefault(),
+);
+
+export {};
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./TSX/App";
+
+import "./TS/scripts/events";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./CSS/index.css";
 import "./SCSS/main.scss";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>,
-    document.getElementById("root"),
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root"),
 );
